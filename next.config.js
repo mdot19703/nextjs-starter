ea338725ea17a4e7174d64e76141a8b99c7093dd
monty <monty@Megatron.local>
2020-07-15 23:23:47 -0400

    Update build config

--- next.config.js
+++ next.config.js
@@ -7,7 +7,26 @@ module.exports = {
   }
 };
 
+const data = require('./utils/projectsData');
 
+module.exports = {
+  exportTrailingSlash: true,
+  exportPathMap: async function () {
+    const { projects } = data;
+    const paths = {
+      '/': { page: '/' },
+    };
+
+    projects.forEach((project) => {
+      paths[`/project/${project.slug}`] = {
+        page: '/project/[path]',
+        query: { path: project.slug },
+      };
+    });
+
+    return paths;
+  },
+};
 
 
 
